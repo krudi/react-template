d2ff692a542ae63143155b6dba54be09726fff56
Patryk Kudlik <patryk.kudlik@gmail.com>
2025-09-18 22:18:31 +0200

    chore(config): integrate shared prettier, eslint, and typescript configs and migrate local configs to .mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    experimental: {
+        globalNotFound: true,
+    },
+    reactStrictMode: true,
+};
+
+export default nextConfig;
